04a72a2f2ffd3f0f12807285fabced9105080ab3
Maxim Lobanov <v-malob@microsoft.com>
2018-10-23 11:13:19 +0300

    Add metrics / custom metrics

--- Sample/Sample/ViewControllers/MetricsViewController.swift
+++ Sample/Sample/ViewControllers/MetricsViewController.swift
@@ -1,9 +1,34 @@
 import UIKit
+import HockeySDK;
 
 class MetricsViewController: UIViewController {
+    @IBOutlet weak var metricsStatus: UISegmentedControl!
+    @IBOutlet weak var eventNameTextBox: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        metricsStatus?.selectedSegmentIndex = BITHockeyManager.shared().isMetricsManagerDisabled ? 1 : 0;
+    }
+    
+    @IBAction func switchMetricsStatus(_ sender: Any) {
+        BITHockeyManager.shared().isMetricsManagerDisabled = metricsStatus.selectedSegmentIndex == 1;
+    }
+    
+    @IBAction func trackEventClicked(_ sender: Any) {
+        let manager = BITHockeyManager.shared().metricsManager;
+        manager.trackEvent(withName: "testEvent");
+    }
+    
+    @IBAction func trackCustomEventClicked(_ sender: Any) {
+        let manager = BITHockeyManager.shared().metricsManager;
+        let eventName = eventNameTextBox?.text ?? "";
+        if (eventName.isEmpty) {
+            let alert = UIAlertController(title: "Incorrect event name", message: "Event name should not be empty", preferredStyle: UIAlertController.Style.alert);
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil));
+            self.present(alert, animated: true, completion: nil);
+            return;
+        }
+        
+        manager.trackEvent(withName: eventName);
     }
 }
